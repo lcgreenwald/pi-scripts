ae5067228741b9a676dd9043dfb602671a6304d3
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-08-11 06:38:52 -0500

    Added update scripts.  Updated README.

--- README.md
+++ README.md
@@ -18,16 +18,23 @@ Run the following commands
 
 This script downloads, compiles and installs the latest version of TrustedQSL.
 
-Run the following command
+Run the following command for the initial installation
 
 	bash $HOME/pi-scripts/tqsl-install.sh
+	
+Run the following command to update TruestedQSL
+
+	bash $HOME/pi-scripts/tqsl-update.sh
 
 # K4CPO-FD-Logger
 
 This script downloads and installs a version of K4CPO-FD-Logger customized for N0SUW/WB0SIO.
 
-Run the following command
+Run the following command for the initial installation
 
 	bash $HOME/pi-scripts/fd-logger-install.sh
 
+Run the following command to update K4CPO-FD-Logger
+
+	bash $HOME/pi-scripts/fd-logger-update.sh
 	
---
+++ fd-logger-update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+clear;echo;echo
+
+MYPATH=$HOME/pi-scripts
+#####################################
+#	notice to user
+#####################################
+cat <<EOF > $MYPATH/intro.txt
+K4CPO-FD-Logger update by wb0sio.
+This script downloads and installs the latest version of 
+the K4CPO-FD-Logger customized for N0SUW/WB0SIO.
+EOF
+
+INTRO=$(yad --width=550 --height=250 --text-align=center --center --title="K4CPO-FD-Logger"  --show-uri \
+--image $LOGO --window-icon=$LOGO --image-on-top --separator="|" --item-separator="|" \
+--text-info<$MYPATH/intro.txt \
+--button="Continue":2 > /dev/null 2>&1)
+BUT=$?
+if [ $BUT = 252 ]; then
+rm $MYPATH/intro.txt
+exit
+fi
+rm $MYPATH/intro.txt
+
+cd #HOME/K4CPO-FD-Logger
+git pull
+sudo cp * /var/www/html/log/
+
+cat <<EOF > $MYPATH/intro.txt
+The K4CPO-FD-Logger update is complete.
+EOF
+
+INTRO=$(yad --width=550 --height=250 --text-align=center --center --title="K4CPO-FD-Logger"  --show-uri \
+--image $LOGO --window-icon=$LOGO --image-on-top --separator="|" --item-separator="|" \
+--text-info<$MYPATH/intro.txt \
+--button="Continue":2 > /dev/null 2>&1)
+BUT=$?
+if [ $BUT = 252 ]; then
+rm $MYPATH/intro.txt
+exit
+fi
+rm $MYPATH/intro.txt
