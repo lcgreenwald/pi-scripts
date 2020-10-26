ccec4f2938873166d89a280cee8ce311d276da2d
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-10-26 10:39:17 -0500

    Update pi-build-test-install.sh

--- pi-build-test-install.sh
+++ pi-build-test-install.sh
@@ -59,7 +59,9 @@ cp -f $MYPATH/bin/*.sh ~/bin/
 cp -f $MYPATH/conky/get-grid ~/bin/conky/
 cp -f $MYPATH/desktop_files/* $HOME/.local/share/applications/
 cp -rf $MYPATH/.local/share/* $HOME/.local/share/
-mkdir $HOME/.xlog/
+if ! -d $HOME/.xlog 2>/dev/null; then
+	mkdir $HOME/.xlog
+fi
 cp -rf $MYPATH/.xlog/* $HOME/.xlog/
 cp -f $MYPATH/.config/* $HOME/.config/
 sed -i "s/km4ack\/hotspot-tools2/lcgreenwald\/autohotspot-tools2/" $HOME/pi-build/update
