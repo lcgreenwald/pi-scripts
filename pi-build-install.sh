ba8d542420f0ce2b2f9c3b428500ba7600bd9418
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-11-05 07:09:16 -0600

    Update pi-build-install.sh

--- pi-build-install.sh
+++ pi-build-install.sh
@@ -61,11 +61,12 @@ cp -f $MYPATH/bin/*.sh ~/bin/
 cp -f $MYPATH/conky/get-grid ~/bin/conky/
 cp -f $MYPATH/desktop_files/* $HOME/.local/share/applications/
 cp -rf $MYPATH/local/share/* $HOME/.local/share/
-if ! -d $HOME/.xlog 2>/dev/null; then
+if [ ! -d $HOME/.xlog 2>/dev/null ] ; then
 	mkdir $HOME/.xlog
 fi
 cp -rf $MYPATH/xlog/* $HOME/.xlog/
 cp -f $MYPATH/config/* $HOME/.config/
+cp -f $MYPATH/conky/.conkyrc $HOME/.conkyrc
 sed -i "s/km4ack\/hotspot-tools2/lcgreenwald\/autohotspot-tools2/" $HOME/pi-build/update
 sed -i "s/km4ack\/hotspot-tools2/lcgreenwald\/autohotspot-tools2/" $HOME/pi-build/functions/base.function
 sed -i "s/pi-build/pi-scripts/" $HOME/.local/share/applications/setconky.desktop
