f2abae0d4bf816a9a79cb9c7cd20743a4d83a945
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-12-12 12:12:05 -0600

    Added k index to conky

--- bin/solar.sh
+++ bin/solar.sh
@@ -4,6 +4,7 @@ wget -N --quiet http://www.hamqsl.com/solarrss.php
 
 sed -n -r -e 's!^.*<solarflux>(.*)</solarflux>.*$!\1!p' solarrss.php > s-flux.txt
 sed -n -r -e 's!^.*<aindex>(.*)</aindex>.*$!\1!p' solarrss.php > a-index.txt
+sed -n -r -e 's!^.*<kindex>(.*)</kindex>.*$!\1!p' solarrss.php > k-index.txt
 sed -n -r -e 's!^.*<sunspots>(.*)</sunspots>.*$!\1!p' solarrss.php > sunspots.txt
 sed -n -r -e 's!^.*<muf>(.*)</muf>.*$!\1!p' solarrss.php > muf.txt
 
