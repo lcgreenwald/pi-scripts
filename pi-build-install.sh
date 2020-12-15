9dee1f7b3a179f22d3d591b806dbd1ff2ca138fd
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-12-15 16:16:49 -0600

    Update pi-build-install.sh

--- pi-build-install.sh
+++ pi-build-install.sh
@@ -167,7 +167,7 @@ Wait for the pi-build-install finished dialog box.
 Please select Master, Beta or Dev installation.
 EOF
 
-INTRO=$(yad --width=700 --height=275 --text-align=center --center --title="Build-a-Pi"  --show-uri \
+INTRO=$(yad --width=750 --height=275 --text-align=center --center --title="Build-a-Pi"  --show-uri \
 --image $LOGO --window-icon=$LOGO --image-on-top --separator="|" --item-separator="|" \
 --text-info<$MYPATH/intro.txt \
 --button="Master":2 > /dev/null 2>&1 \
