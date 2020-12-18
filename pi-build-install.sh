0432b2639fa8b906655fe18e66879b36b2b8f3c0
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-12-18 13:49:18 -0600

    Update pi-build-install.sh

--- pi-build-install.sh
+++ pi-build-install.sh
@@ -72,7 +72,7 @@ if ! hash log2ram 2>/dev/null; then
 fi
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y bluetooth
+sudo apt install -y bluetooth bluez-cups bluez-obexd
 if ! hash yad 2>/dev/null; then
 	sudo apt install -y yad
 fi
