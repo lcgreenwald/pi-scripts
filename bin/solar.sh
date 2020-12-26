b0b4255b488021bf721251f7c5e20fc40f9c6e0e
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-12-26 13:01:29 -0600

    Updates for solar conditions gif

--- bin/solar.sh
+++ bin/solar.sh
@@ -6,12 +6,13 @@
 # https://groups.io/g/KM4ACK-Pi/topic/adding_solar_flux_and_a_index/78788335?p=,,,20,0,0,0::recentpostdate%2Fsticky,,,20,2,0,78788335
 # and updated by G.A.(Jerry) Minor N5RKE
 # modified 2020/12/16 - Larry Greenwald WB0SIO - Added muf
+#          2020/12/26 - Larry Greenwald WB0SIO - Per www.hamqsl.com data is updated every 30 minutes
 ##############################
 
 cd ~/bin/conky/solardata || exit 1
 
-# Stop if one of the output files exist and is newer than 60 minutes
-if [[ -r s-flux.txt && $(( $(date +%s) - $(stat -c "%Y" s-flux.txt) )) -lt 3600 ]]
+# Stop if one of the output files exist and is newer than 30 minutes
+if [[ -r s-flux.txt && $(( $(date +%s) - $(stat -c "%Y" s-flux.txt) )) -lt 1800 ]]
 then
     exit 0
 fi
--- bin/solarimage.sh
+++ bin/solarimage.sh
@@ -3,7 +3,18 @@
 #for use in conky. It should be run by cron once
 #per hour
 #10DEC2020 KM4ACK
-cd ~/Downloads
+##############################
+# modified 2020/12/26 - Larry Greenwald WB0SIO - Per www.hamqsl.com data is updated every 30 minutes
+##############################
+
+cd ~/bin/conky/solardata || exit 1
+
+# Stop if one of the output files exist and is newer than 30 minutes
+if [[ -r s-flux.txt && $(( $(date +%s) - $(stat -c "%Y" solarpich.gif) )) -lt 1800 ]]
+then
+    exit 0
+fi
+
 wget http://www.hamqsl.com/solarpich.php
 mv solarpich.php solarpich.gif
 wget http://www.hamqsl.com/solarmuf.php
