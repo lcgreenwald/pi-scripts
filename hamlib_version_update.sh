b9da00b12f7bbf0ce5dcd39282e05f2d318adb54
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2020-12-29 16:09:47 -0600

    Hamlib fixes

--- hamlib_version_update.sh
+++ hamlib_version_update.sh
@@ -7,12 +7,11 @@
 ############
 
 # Get the current and latest version numbers
-RIG=$(rigctl --version | grep Hamlib | sed s/"rigctl(d),\ Hamlib\ "//)
+OLDRIG=$(cat $HOME/pi-build/functions/base.function | grep -o hamlib-[0-9].[0-9] | grep -o [0-9].[0-9] | head -n 1)
 NEWRIG=$(curl -s https://sourceforge.net/projects/hamlib/files/latest/download | \
 grep -o https://downloads.sourceforge.net/project/hamlib/hamlib/[0-9].[0-9] | \
 head -n 1 | awk -F "/" '{print $7}') 
 
 # Update pi-build/functions/base.function to install the latest version.
-sed -i "s/hamlib\/$RIG/hamlib\/$NEWRIG/" $HOME/pi-build/functions/base.function
-sed -i "s/hamlib-$RIG/hamlib-$NEWRIG/" $HOME/pi-build/functions/base.function
-
+sed -i "s/hamlib\/$OLDRIG\/hamlib/hamlib\/$NEWRIG\/hamlib/" $HOME/pi-build/functions/base.function
+sed -i "s/hamlib-$OLDRIG/hamlib-$NEWRIG/" $HOME/pi-build/functions/base.function
