5a082118964fa811232bdb914e37c099ef37ad7f
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-01-06 06:54:09 -0600

    Conky updates

--- bin/writefreq.sh
+++ bin/writefreq.sh
@@ -6,18 +6,6 @@
 #20200428 modified
 #20201022 wb0sio - edited to use rigctl to read the rig frequency.
 
-MAIN () {
 
 FREQ=$(rigctl -m2 f)
-FREQCH=$(echo "$FREQ" | grep error)
-
-if [ -z "$FREQCH" ]
-then
 echo ${FREQ:0:-3}/1000 | bc -l | xargs printf "%.3f ${FREQ: -3}" > /home/pi/bin/conky/Freq.txt
-else
-MAIN "$1"
-fi
-
-}
-
-MAIN
