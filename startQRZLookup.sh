909e081d2dab6c852a0fd930b6cb01f7e81b30d1
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-01-26 07:01:04 -0600

    Added QRZLookup
    Misc minor changes.

---
+++ startQRZLookup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##################################
+#	Execute QRZLookup by m0iax
+# Call sign lookup for JS8Call
+# 2021/01/26 wb0sio
+##################################
+
+cd $HOME/QRZLookup
+./QRZLookup.py
