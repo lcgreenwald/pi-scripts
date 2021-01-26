ffcb3d55e05031ec01fd454f984a2b1b19cbbcb3
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-01-26 07:10:00 -0600

    Removed QRZLookup

--- startQRZLookup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-##################################
-#	Execute QRZLookup by m0iax
-# Call sign lookup for JS8Call
-# 2021/01/26 wb0sio
-##################################
-
-cd $HOME/QRZLookup
-./QRZLookup.py
