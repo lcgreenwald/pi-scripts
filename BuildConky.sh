6be8f620bf375bf5677273ddfb7845896dc7cf25
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-04-15 06:39:35 -0500

    conky window type updates

--- BuildConky.sh
+++ BuildConky.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
+sudo apt-get install -y cmake libimlib2-dev libncurses5-dev libx11-dev libxdamage-dev libxft-dev libxinerama-dev libxml2-dev libxext-dev libcurl4-openssl-dev liblua5.3-dev
 cd
 git clone https://github.com/brndnmtthws/conky.git
 cd conky
