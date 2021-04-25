d5e34832a17da62b0f9be311df9143d18ffffe70
lcgreenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-04-25 07:24:21 -0500

    Update pi-build-install.sh
    removed Hamlib version BAP update

--- pi-build-install.sh
+++ pi-build-install.sh
@@ -234,21 +234,6 @@ sed -i "s/km4ack\/pi-scripts\/master\/gpsinstall/lcgreenwald\/pi-scripts\/master
 sed -i '/#reboot when done/a exit' $HOME/pi-build/build-a-pi
 sed -i '/#reboot when done/a exit' $HOME/pi-build/update
 
-############
-# Update build-a-pi base.function to install the latest version of HamLib
-# Created: 2020/12/29 - Larry Greenwald - WB0SIO
-############
-
-# Get the current and latest version numbers
-OLDRIG=$(cat $HOME/pi-build/functions/base.function | grep -o hamlib-[0-9].[0-9] | grep -o [0-9].[0-9] | head -n 1)
-NEWRIG=$(curl -s https://sourceforge.net/projects/hamlib/files/latest/download | \
-grep -o https://downloads.sourceforge.net/project/hamlib/hamlib/[0-9].[0-9] | \
-head -n 1 | awk -F "/" '{print $7}') 
-
-# Update pi-build/functions/base.function to install the latest version.
-sed -i "s/hamlib\/$OLDRIG\/hamlib/hamlib\/$NEWRIG\/hamlib/" $HOME/pi-build/functions/base.function
-sed -i "s/hamlib-$OLDRIG/hamlib-$NEWRIG/" $HOME/pi-build/functions/base.function
-
 # Run build-a-pi
 bash pi-build/build-a-pi
 
