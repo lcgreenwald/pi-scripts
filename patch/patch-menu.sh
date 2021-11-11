868712dd96f3bb61890b186aa2bed214586aa2c6
Larry Greenwald <33462239+lcgreenwald@users.noreply.github.com>
2021-11-11 06:07:27 -0600

    Update patch-menu.sh

--- patch/patch-menu.sh
+++ patch/patch-menu.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-#script to test a patch system for quick fixes in Pi-Scripts
+#script for a patch system for quick fixes in Pi-Scripts
 #KM4ACK 20210805
 #WB0SIO 20211031
 
-#MYPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-#LOGO=${MYPATH}/logo.png
-#DIR=/run/user/$UID
-#PATCH=${MYPATH}/patch.txt
 PATCHDIR=/run/user/${UID}/patch
-echo "MYPATH = $MYPATH"
-echo "PATCH = $PATCH"
 
-#cat $PATCHDIR/avail-patch.txt
 source $PATCHDIR/avail-patch.txt
 
        
@@ -24,7 +17,7 @@ yad --center --list --checklist --width=750 --height=750 --separator="" \
 --window-icon=${LOGO} --image-on-top --text-align=center \
 --text="<b>Patches</b>" --title="Pi-Scripts Install" \
 false "hamlib4dot3dot1date20211105" "$hamlib4dot3dot1date20211105" "HamLib update" \
-false "testpatch20211020" "$testpatch20211020" "Another small test application" \
+#false "testpatch20211020" "$testpatch20211020" "Another small test application" \
 --button="Exit":1 \
 --button="Check All and Continue":3 \
 --button="Install Selected":2 > ${PATCH}
