ec81d02f0a6ed0050691394f2e684c84e221b3e5
Larry Greenwald <33462239+lcgreenwald@users.noreply.github.com>
2022-01-21 05:24:26 -0600

    Update update-ssd.sh

--- update-ssd.sh
+++ update-ssd.sh
@@ -130,7 +130,7 @@ clear
 #####################################
 #Scan system for updated applications
 #####################################
-yad  --width=550 --height=150 --text-align=center --center --title="Update" \
+yad  --width=550 --height=150 --text-align=center --center --title="Update-ssd" \
 --image ${LOGO} --window-icon=${LOGO} --image-on-top --separator="|" --item-separator="|" \
 --text="<b>Version ${VERSION}</b>\r\r\First we need to scan the system to see what is installed. \
 This should take less than a minute. Ready when you are." \
