7829b3c344317ca57b5869fe586464c066eedc38
Larry Greenwald <33462239+lcgreenwald@users.noreply.github.com>
2023-08-11 08:19:31 -0500

    Update FT817-rigctld.sh

--- bin/FT817-rigctld.sh
+++ bin/FT817-rigctld.sh
@@ -4,4 +4,7 @@
 #rigctld -m 1022 -r /dev/rfcomm0 -s 38400
 # USB cable
 #rigctld -m 1022 -r /dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0 -s 38400
-rigctld -m 1022 -r /dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0 -s 9600
+#rigctld -m 1022 -r /dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0 -s 9600
+rigctld -m 1022 -r /dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_b06b862acbb1eb118516bd7718997a59-if00-port0 -s 38400
+#rigctld -m 1022 -r /dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_b06b862acbb1eb118516bd7718997a59-if00-port0 -s 9600
+
